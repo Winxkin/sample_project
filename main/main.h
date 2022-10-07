2842af140c94c0d35a48dec875fe109dfde4acd6
Winxkin <huan2931@gmail.com>
2022-10-08 00:23:22 +0700

    v1.0

--- main/main.h
+++ main/main.h
@@ -3,9 +3,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+/*Freertos */
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/event_groups.h"
+
+/*smartconfig wifi */
 #include "esp_wifi.h"
 #include "esp_wpa2.h"
 #include "esp_event.h"
@@ -14,17 +17,5 @@
 #include "nvs_flash.h"
 #include "esp_netif.h"
 #include "esp_smartconfig.h"
-#include "delay.h"
-
-#define WIFI_CONNECTED 1
-#define WIFI_STATUS_SET 0
-
-static int smartconfig_status;
-
-static EventGroupHandle_t s_wifi_event_group;
-
-static const int CONNECTED_BIT = BIT0;
-static const int ESPTOUCH_DONE_BIT = BIT1;
-static const char *WIFI = "smartconfig wifi";
 
 #endif
